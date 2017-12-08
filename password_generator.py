5f38f24d934c7f8ccfd7de66bbf13698c45c9bb6
pavakpaul <pavakatubuntu@gmail.com>
2017-12-08 13:19:37 +0530

    added checking of command line arguments

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python3
 from sys import argv
-from dependencies import getPassword
+from dependencies import getPassword, isInt
 
-if len(argv) == 2:
+if len(argv) == 2 and isInt(argv[1]):
     print (getPassword(argv[1]))
-elif len(argv) == 1:
+else:
     length = input('Please enter the password length: ')
     print(getPassword(length))
