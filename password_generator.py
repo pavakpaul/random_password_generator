ac710ae4795410ad5675bc2c01a7273a20eef969
pavakpaul <pavakatubuntu@gmail.com>
2017-12-12 14:14:54 +0530

    added number of passwords to be generated

--- password_generator.py
+++ password_generator.py
@@ -4,6 +4,10 @@ from dependencies import getPassword, isInt
 
 if len(argv) == 2 and isInt(argv[1]):
     print (getPassword(argv[1]))
+
+elif len(argv) == 3 and isInt(argv[1]) and isInt(argv[2]):
+    for i in range(0, int(argv[2])):
+        print(getPassword(argv[1]))
 else:
     length = input('Please enter the password length: ')
     print(getPassword(length))
